69f2fcf2e5b29999fa30d40871660881d55acebe
ngoeldner <ngoeldner@hotmail.com>
2019-11-01 16:31:09 -0300

    melhorei o rep5

--- repressilator_5.m
+++ repressilator_5.m
@@ -1,84 +1,40 @@
 clear;
 
-dt=0.01; 
-t=0:dt:800; 
+dt=0.05; 
+t=0:dt:400; 
 
 alfa0 = 0.03;
 alfa = 298.2;
 beta = 0.2;
 n = 2;
 
-m1 = 0;
-m2 = 0;
-m3 = 0;
-m4 = 0;
-m5 = 0;
-p1 = 0;
-p2 = 5;
-p3 = 15;
-p4 = 0;
-p5 = 0;
+ms = zeros(1,5);
+ps = zeros(1,5); ps(2) = 5; ps(3) = 15;
 
-m1s = zeros(1, length(t));
-m2s = zeros(1, length(t));
-m3s = zeros(1, length(t));
-m4s = zeros(1, length(t));
-m5s = zeros(1, length(t));
-p1s = zeros(1, length(t));
-p2s = zeros(1, length(t));
-p3s = zeros(1, length(t));
-p4s = zeros(1, length(t));
-p5s = zeros(1, length(t));
-
-s = zeros(1, length(t));
+mcons = zeros(5, length(t));
+pcons = zeros(5, length(t));
 
 for i = 1:length(t)
-  dm1dt = alfa0 .+ (alfa ./ (1 .+ p5.^n)) .- m1;
-  m1 = dm1dt * dt + m1;
-  m1s(i) = m1;
+  for j = 1:5
+%    mRNA concentration
+    dmdt = alfa0 + (alfa ./ (1 .+ ps(1 + mod((j+3), 5)) .^n)) - ms(j);
+    ms(j) = dmdt * dt + ms(j);
+    mcons(j,i) = ms(j);
+  endfor
   
-  dm2dt = alfa0 .+ (alfa ./ (1 .+ p1.^n)) .- m2;
-  m2 = dm2dt * dt + m2;
-  m2s(i) = m2;
-  
-  dm3dt = alfa0 .+ (alfa ./ (1 .+ p2.^n)) .- m3;
-  m3 = dm3dt * dt + m3;
-  m3s(i) = m3;
-  
-  dm4dt = alfa0 .+ (alfa ./ (1 .+ p3.^n)) .- m4;
-  m4 = dm4dt * dt + m4;
-  m4s(i) = m4;
-  
-  dm5dt = alfa0 .+ (alfa ./ (1 .+ p4.^n)) .- m5;
-  m5 = dm5dt * dt + m5;
-  m5s(i) = m5;
-  
-  dp1dt = (beta .* m1 .- beta .* p1);
-  p1 = dp1dt * dt + p1;
-  p1s(i) = p1;
-  
-  dp2dt = beta .* m2 .- beta .* p2;
-  p2 = dp2dt * dt + p2;
-  p2s(i) = p2;
-  
-  dp3dt = (beta .* m3 .- beta .* p3);
-  p3 = dp3dt * dt + p3;
-  p3s(i) = p3;
-  
-  dp4dt = (beta .* m4 .- beta .* p4);
-  p4 = dp4dt * dt + p4;
-  p4s(i) = p4;
-  
-  dp5dt = (beta .* m5 .- beta .* p5);
-  p5 = dp5dt * dt + p5;
-  p5s(i) = p5;
+  for j = 1:5
+%    protein concentration
+    dpdt = (beta .* ms(j) .- beta .* ps(j));
+    ps(j) = dpdt * dt + ps(j);
+    pcons(j,i) = ps(j);
+  endfor
   
 endfor
 
 figure;
 hold on;
 grid on;
-
-plot(t, p1s, 'm;p1;', t, p2s, 'g;p2;', t, p3s, 'b;p3;', t, p4s, 'k;p4;', t, p5s, 'r;p5;');
+plot(t, pcons(1,:), 'g;P1;',t, pcons(2,:), 'b;P2;',t, pcons(3,:), 'm;P3;',t, pcons(4,:), 'k;P4;',t, pcons(5,:), 'c;P5;');
+plot(t,pcons(1,:));
 xlabel('t');
 ylabel('Concentration');
