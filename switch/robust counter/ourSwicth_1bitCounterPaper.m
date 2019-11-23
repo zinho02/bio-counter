f6d6127e44cfddb63d289a2e41344e9b3b6f36bd
ngoeldner <ngoeldner@hotmail.com>
2019-11-23 20:18:52 -0300

    imagens artigo

--- switch/robust counter/ourSwicth_1bitCounterPaper.m
+++ switch/robust counter/ourSwicth_1bitCounterPaper.m
@@ -51,31 +51,42 @@ for i = 2:length(t)
   
   div = 1 + p1/kp1 + p2/kp2 + (p1*p2)/(kp1*kp2);
   
+%  i1
   dpdt = (  km1  *pulso(i) * ((1 + p1 / (kp1)) / div)     /   (1 + (ii2 / (1 + p1s(i) ) ) ^ beta2)   ) - ii1;
   ii1 = dpdt * dt + ii1;
   i1s(i) = ii1; 
   
- dpdt = (  km2  *pulso(i) * ((1 + p2 / (kp2)) / div)       /              (1 + (ii1 / (1 + p2s(i) ) ) ^ gama2)     ) - ii2;
+%  i2  
+  dpdt = (  km2  *pulso(i) * ((1 + p2 / (kp2)) / div)       /              (1 + (ii1 / (1 + p2s(i) ) ) ^ gama2)     ) - ii2;
   ii2 = dpdt * dt + ii2;
   i2s(i) = ii2; 
  
+% p1
   dpdt = (alpha1 / (1 + (p2 / (1 + i2s(i) ) ) ^ beta )) - p1;
   p1 = dpdt * dt + p1;
   p1s(i) = p1;
-  
+
+% p2  
   dpdt = (alpha2 / (1 + (p1 / (1 + i1s(i)) ) ^ gama)) - p2;
   p2 = dpdt * dt + p2;
   p2s(i) = p2;
 
 endfor
 
+%figure;
+%hold on;
+%plot(t, p1s, 'b;R1;', t ,p2s, 'r;R2;', t , i2s, 'm;i2;', t ,i1s,'g;i1;', t , pulso , 'k;Pulse;');
+%xlabel('Time');
+%ylabel('Concentration');
+
 figure;
 hold on;
-grid on;
-%plot(t, p1s, 'b;P1;', t ,p2s, 'r;P2;', t , pulso , 'k;pulso;');
-%plot(t, p1s, 'b;R1;', t ,p2s, 'r;R2;', t , i2s, 'm;i2;', t ,i1s,'g;i1;', t , pulso , 'k;Pulse;');
-%plot(t, p1s, ':k;R1;', t , p2s, '--k;R2;', t , pulso , '-k;Pulse;');
-plot(t,i1s, ':k;i1;', t , i2s, '--k;i2;', t , pulso , '-k;Pulse;');
-%plot(t, p1s, ':k;R1;', t , i2s, '-.k;i2;', t ,i1s,'--k;i1;', t , pulso , '-k;Pulse;');
+plot(t, p1s, ':k;R1;', t , p2s, '-k;R2;', t , pulso , '--k;Pulse;');
+xlabel('Time');
+ylabel('Concentration');
+
+figure;
+hold on;
+plot(t,i1s, 'k;i1;', t , i2s, ':k;i2;', t , pulso , '--k;Pulse;');
 xlabel('Time');
 ylabel('Concentration');
